8b651a5b9b401f7956f665fc19449f63b83ad7ce
Fabian Lohauß <fabian@lohauss.de>
2023-01-14 12:31:42 +0000

    wip

---
+++ cmdlets/UnitTests/Get-DfProjectRootFolder.Tests.ps1
@@ -0,0 +1,11 @@
+BeforeAll {
+    Import-Module $PSScriptRoot/src/DeploymentFramework.psm1 -force
+}
+
+Describe "Get-DfProjectRootFolder" {
+    Context "not project root" {
+        It "should throw" {
+            Get-DfProjectRootFolder | Should -Throw -ExpectedMessage "Failed to find DeploymentFramework project root folder"
+        }
+    }
+}
