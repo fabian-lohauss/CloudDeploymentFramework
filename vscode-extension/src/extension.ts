5f4fd838fe54a9808df4b68244a77778bd8b5a3c
Fabian Lohauß <fabian@lohauss.de>
2023-04-29 18:57:23 +0000

    new example extension

--- vscode-extension/src/extension.ts
+++ vscode-extension/src/extension.ts
@@ -1,20 +1,26 @@
-'use strict';
-
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-import { DepNodeProvider } from './nodeDependencies';
-import { Dependency } from "./Dependency";
-
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	const rootPath = (vscode.workspace.workspaceFolders && (vscode.workspace.workspaceFolders.length > 0))
-		? vscode.workspace.workspaceFolders[0].uri.fsPath : undefined;
 
-	// Samples of `window.registerTreeDataProvider`
-	const nodeDependenciesProvider = new DepNodeProvider(rootPath);
-	vscode.window.registerTreeDataProvider('nodeDependencies', nodeDependenciesProvider);
-	vscode.commands.registerCommand('nodeDependencies.refreshEntry', () => nodeDependenciesProvider.refresh());
-	vscode.commands.registerCommand('extension.openPackageOnNpm', moduleName => vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(`https://www.npmjs.com/package/${moduleName}`)));
-	vscode.commands.registerCommand('nodeDependencies.addEntry', () => vscode.window.showInformationMessage(`Successfully called add entry.`));
-	vscode.commands.registerCommand('nodeDependencies.editEntry', (node: Dependency) => vscode.window.showInformationMessage(`Successfully called edit entry on ${node.label}.`));
-	vscode.commands.registerCommand('nodeDependencies.deleteEntry', (node: Dependency) => vscode.window.showInformationMessage(`Successfully called delete entry on ${node.label}.`));
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "deploymentframework" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('deploymentframework.helloWorld', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello World from DeploymentFramework!');
+	});
+
+	context.subscriptions.push(disposable);
 }
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
--- vscode-extension/src/test/Dependency.test.ts
+++
@@ -1,6 +0,0 @@
-import { Dependency } from "../Dependency";
-import * as vscode from 'vscode';
-
-test('create dependency', () => {
-  expect(new Dependency('moduleName', 'version', vscode.TreeItemCollapsibleState.Collapsed)).not.toBeNull();
-});
