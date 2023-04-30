94e126e63680f5626272b7bedff7345d7bea0853
Fabian Lohauß <fabian@lohauss.de>
2023-04-30 11:17:00 +0000

    wip

--- vscode-extension/src/extension.ts
+++ vscode-extension/src/extension.ts
@@ -1,26 +1,61 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "deploymentframework" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('deploymentframework.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from DeploymentFramework!');
-	});
-
-	context.subscriptions.push(disposable);
+export class FruitItem extends vscode.TreeItem {
+	constructor(
+		public readonly label: string,
+		public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+		public readonly command?: vscode.Command
+	) {
+		super(label, collapsibleState);
+	}
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export class FruitDataProvider implements vscode.TreeDataProvider<FruitItem> {
+	private _onDidChangeTreeData: vscode.EventEmitter<FruitItem | undefined | null | void> = new vscode.EventEmitter<FruitItem | undefined | null | void>();
+	readonly onDidChangeTreeData: vscode.Event<FruitItem | undefined | null | void> = this._onDidChangeTreeData.event;
+
+	getTreeItem(element: FruitItem): vscode.TreeItem {
+		return element;
+	}
+
+	getChildren(element?: FruitItem): Thenable<FruitItem[]> {
+		if (element) {
+			return Promise.resolve(this.getFruits(element.label));
+		} else {
+			return Promise.resolve(this.getTypes());
+		}
+	}
+
+	private getTypes(): FruitItem[] {
+		return [
+			new FruitItem('Citrus', vscode.TreeItemCollapsibleState.Collapsed),
+			new FruitItem('Berries', vscode.TreeItemCollapsibleState.Collapsed)
+		];
+	}
+
+	private getFruits(type: string): FruitItem[] {
+		const fruits = {
+			citrus: ['Orange', 'Lemon', 'Grapefruit'],
+			berries: ['Strawberry', 'Blueberry', 'Raspberry']
+		};
+
+		//  array of fruit items
+		let result: FruitItem[] = [];
+		if (type === 'Citrus') {
+			result = fruits.citrus.map( 
+				(fruit: string) => new FruitItem(fruit, vscode.TreeItemCollapsibleState.None)
+			);
+
+		} else if (type === 'Berries') {
+			result = fruits.berries.map(
+				(fruit: string) => new FruitItem(fruit, vscode.TreeItemCollapsibleState.None)
+			);
+		}
+		return result;
+	}
+}
+
+export function activate(context: vscode.ExtensionContext) {
+	const fruitDataProvider = new FruitDataProvider();
+	vscode.window.registerTreeDataProvider('sampleTreeView', fruitDataProvider);
+}
