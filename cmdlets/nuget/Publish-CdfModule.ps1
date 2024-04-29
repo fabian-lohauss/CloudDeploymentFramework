b1bb109ccf34a657f2583f1cda530d1921180106
Fabian Lohauß <fabianl@microsoft.com>
2024-04-29 10:13:58 +0000

    wip

--- cmdlets/nuget/Publish-CdfModule.ps1
+++ cmdlets/nuget/Publish-CdfModule.ps1
@@ -4,36 +4,44 @@ param(
 )
 
 $NugetFolder = $PSScriptRoot
-$SourceFolder = Join-Path $NugetFolder -ChildPath ../src/CloudDeploymentFramework
+Write-Host ("Nuget folder: '{0}'" -f $NugetFolder)
+Write-Host ("Getting module source folder")
+$SourceFolder = Join-Path $NugetFolder -ChildPath ../src/CloudDeploymentFramework -Resolve
+Write-Host ("Module source folder: '{0}'" -f $SourceFolder)
 
+Write-Host "Getting latest prerelease version of CloudDeploymentFramework module from PSGallery"
 $PublishedModule = Find-PSResource CloudDeploymentFramework -Prerelease -Repository PSGallery
-$CurrentVersion = $PublishedModule.Version
-Write-Host "Current version: $CurrentVersion"
+$LatestPrereleaseVersion = $PublishedModule.Version
+Write-Host "Latest prerelease version: $LatestPrereleaseVersion"
 
-$NewVersion = [Version]::new($CurrentVersion.Major, $CurrentVersion.Minor, $CurrentVersion.Build + 1)
-Write-Host "New version: $NewVersion"
+$NewPrereleaseVersion = [Version]::new($LatestPrereleaseVersion.Major, $LatestPrereleaseVersion.Minor, $LatestPrereleaseVersion.Build + 1)
+Write-Host "New version: $NewPrereleaseVersion"
 
 $Psd1File = Join-Path $SourceFolder -ChildPath CloudDeploymentFramework.psd1
 $Psd1Content = Get-Content $Psd1File
 
-$CurrentVersionLine = $Psd1Content | Where-Object { $_ -Match '\s*ModuleVersion\s*=\s*' }
-$NewVersionLine = ("    ModuleVersion = '{0}'" -f $NewVersion)
+$CurrentPsd1VersionLine = $Psd1Content | Where-Object { $_ -Match '\s*ModuleVersion\s*=\s*' }
+$NewPsd1VersionLine = ("    ModuleVersion = '{0}'" -f $NewPrereleaseVersion)
 
-$Psd1Content -replace $CurrentVersionLine, $NewVersionLine | Out-File $Psd1File
+$Psd1Content -replace $CurrentPsd1VersionLine, $NewPsd1VersionLine | Out-File $Psd1File
 
 $LocalRepositoryName = "LocalRepository"
 $LocalRepositoryPath = Join-Path $NugetFolder -ChildPath "Repository" -AdditionalChildPath $LocalRepositoryName
+Write-Host ("Local repository path: '{0}'" -f $LocalRepositoryPath)
 New-Item -Path $LocalRepositoryPath -ItemType Directory -Force | Out-Null
 if (Get-PSResourceRepository -Name $LocalRepositoryName -ErrorAction SilentlyContinue) {
+    Write-Host ("Updating local repository '{0}' with Uri '{1}'" -f $LocalRepositoryName, $LocalRepositoryPath)
     Set-PSResourceRepository -Name $LocalRepositoryName -Uri $LocalRepositoryPath -Trusted
 }
 else {
+    Write-Host ("Registering new local repository '{0}' with Uri '{1}'" -f $LocalRepositoryName, $LocalRepositoryPath)
     Register-PSResourceRepository -Name $LocalRepositoryName -Uri $LocalRepositoryPath -Trusted
 }
-Write-Host "Publishing module to $LocalRepositoryName"
+
+Write-Host ("Publishing module in version '{0}' to '{1}'" -f $NewPrereleaseVersion, $LocalRepositoryName)
 Publish-PSResource -Path $SourceFolder -ApiKey "abc" -Repository $LocalRepositoryName -Verbose
 
-Write-Host "Publishing module to PSGallery"
+Write-Host ("Publishing module in version '{0}' to PSGallery" -f $NewPrereleaseVersion)
 Publish-PSResource -Path $SourceFolder -ApiKey $NuGetApiKey -Repository "PSGallery" -Verbose
 
 
