ad4b9ab0f452c08be8872d5efce75ea0c2117237
Fabian Lohauß <fabianl@microsoft.com>
2024-04-29 10:30:57 +0000

    wip

--- cmdlets/nuget/Publish-CdfModule.ps1
+++ cmdlets/nuget/Publish-CdfModule.ps1
@@ -17,7 +17,7 @@ Write-Host "Latest prerelease version: $LatestPrereleaseVersion"
 $NewPrereleaseVersion = [Version]::new($LatestPrereleaseVersion.Major, $LatestPrereleaseVersion.Minor, $LatestPrereleaseVersion.Build + 1)
 Write-Host "New version: $NewPrereleaseVersion"
 
-Write-Host ("NEW_VERSION={0}" -f $NewPrereleaseVersion) >> "$GITHUB_OUTPUT"
+Write-Host ("NEW_VERSION={0}" -f $NewPrereleaseVersion) >> "$env:GITHUB_OUTPUT"
 
 
 $Psd1File = Join-Path $SourceFolder -ChildPath CloudDeploymentFramework.psd1
